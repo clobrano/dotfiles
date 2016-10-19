19e87c634b7cb4b04e3928d4cbc73a7bdfb86f81
Carlo Lobrano <c.lobrano@gmail.com>
2016-10-19 23:22:41 +0200

    Conditioned neovim install

--- neovim/install.sh
+++ neovim/install.sh
@@ -2,10 +2,16 @@
 # -*- coding: UTF-8 -*-
 set -e
 set -x
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo apt-get update
-sudo apt-get install neovim
-sudo apt-get install python-dev python-pip python3-dev python3-pip
 
-ln -s $(pwd)/nvim $HOME/.config/nvim
+which nvim > /dev/null
+
+if [ "$?" = 1 ]; then
+  sudo apt-get install software-properties-common
+  sudo add-apt-repository ppa:neovim-ppa/unstable
+  sudo apt-get update
+  sudo apt-get install neovim
+  sudo apt-get install python-dev python-pip python3-dev python3-pip
+else
+  ln -s $(pwd)/nvim $HOME/.config/nvim
+fi
+curl -fLo "Ubuntu Mono for Powerline Nerd Font Complete.ttf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/UbuntuMono/Regular/complete/Ubuntu%20Mono%20derivative%20Powerline%20Nerd%20Font%20Complete%20Mono.ttf
