13220dc058f424068f4dfa892b1c02727ccdb9cf
Carlo Lobrano <carlo.lobrano@telit.com>
2016-11-17 16:06:36 +0100

    Added modemmanager start and restart shortcuts

--- bash/modemmanager_aliases.sh
+++ bash/modemmanager_aliases.sh
@@ -1,3 +1,4 @@
 alias mm-stop='sudo systemctl stop ModemManager'
-alias mm-start='sudo systemctl restart ModemManager'
+alias mm-start='sudo systemctl start ModemManager'
+alias mm-restart='sudo systemctl daemon-reload && sudo systemctl restart ModemManager'
 alias mm-log='journalctl -fu ModemManager | raffaello -p modemmanager'
