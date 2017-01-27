eddde9ca5382b3ee5265fa25d85115e3b96a1d6d
Carlo Lobrano <carlo.lobrano@telit.com>
2017-01-27 09:30:39 +0100

    Added new aliases

--- bash/modemmanager_aliases.sh
+++ bash/modemmanager_aliases.sh
@@ -1,4 +1,5 @@
 alias mm-stop='sudo systemctl stop ModemManager'
 alias mm-start='sudo systemctl start ModemManager'
 alias mm-restart='sudo systemctl daemon-reload && sudo systemctl restart ModemManager'
+alias mm-status='sudo systemctl status ModemManager'
 alias mm-log='journalctl -fu ModemManager | raffaello -p modemmanager'
