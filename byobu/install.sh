2a8f036af4a26536dc3363cbdfb2cb9ee17aea54
Carlo Lobrano <c.lobrano@gmail.com>
2017-06-07 09:23:40 +0200

    byobu: added target for plugin download

--- byobu/install.sh
+++ byobu/install.sh
@@ -2,13 +2,10 @@
 # -*- coding: UTF-8 -*-
 set -u
 PWD=$(pwd)
+BYOBU_HOME=$HOME/.byobu
 files="keybindings.tmux .tmux.conf status"
 
 for f in $files; do
     cp $HOME/.byobu/$f $HOME/.byobu/$f.bak
     ln -sf "$PWD"/$f $HOME/.byobu/$f
 done
-#KEYBINDINGS=keybindings.tmux
-#
-#cp $HOME/.byobu/$KEYBINDINGS $HOME/.byobu/$KEYBINDINGS.bak
-#ln -sf "$PWD"/$KEYBINDINGS $HOME/.byobu/$KEYBINDINGS
