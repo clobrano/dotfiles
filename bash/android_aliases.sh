43ac3b1dd748bc87a9e3fdd299b093f8025aeb34
Carlo Lobrano <carlo.lobrano@telit.com>
2017-09-07 14:03:33 +0200

    changed gopath and added scan-build aliases

---
+++ bash/android_aliases.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# -*- coding: UTF-8 -*-
+alias scp-nougat='scp carlolo@10.255.252.137:/home/carlolo/external/androidx86/nougat2/out/target/product/x86/android_x86.iso /home/carlolo/Downloads/nougat.iso'
