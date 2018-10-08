c1e201432d1a80100daaa4f0a38a03cee58ef7c4
Carlo Lobrano <c.lobrano@gmail.com>
2018-10-08 19:55:20 +0200

    updates

--- yaru/yaru.sh
+++ yaru/yaru.sh
@@ -5,4 +5,4 @@ sudo apt install -y libgtk-3-dev gtk-3-examples meson sassc
 
 gsettings set org.gtk.Settings.Debug enable-inspector-keybinding true
 
-# update-alternatives --install /usr/share/gnome-shell/theme/gdm3.css gdm3.css /usr/local/share/gnome-shell/theme/Yaru/gnome-shell.css 15
+# update-alternatives --install /usr/share/gnome-shell/theme/gdm3.css gdm3.css /usr/share/gnome-shell/theme/Yaru/gnome-shell.css 15
